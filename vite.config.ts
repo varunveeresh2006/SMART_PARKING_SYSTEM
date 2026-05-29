e00101d03572fc0e23f62d46183e31d7c77caa06
Varun V <varunveeresh43@gmail.com>
2026-05-29 20:22:55 +0530

    Set base URL for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/varunveeresh2006/SMART_PARKING_SYSTEM',
   optimizeDeps: {
     include: [
       'date-fns',
